a678c7071f134959551aa7a67c6a355b3729b5ab
Prachi Kushwaha <prachiku673@gmail.com>
2024-05-16 14:00:19 +0530

    how react-work

---
+++ App.js
@@ -0,0 +1,32 @@
+{/* <div id="parent">
+    <div id="child">
+        <h1>hello pri</h1>
+    </div>
+</div> */}
+
+// const heading = React.createElement("h1", {}, "hello world from react")
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+
+const heading = React.createElement(
+    "div",
+    {id:"parent"},[
+        
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1", {}, "hello pri" ),
+        React.createElement("h2", {}, "hello pri2" )]
+        ),
+        
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [React.createElement("h1", {}, "hello pri" ),
+        React.createElement("h2", {}, "hello pri2" )]
+        )
+    ]
+ )
+ console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
